fa7158119efa3dd288c28d35f49a925efd46796b
Praveen Arimbrathodiyil <praveen@debian.org>
2013-06-13 20:46:04 +0530

    remove dh_make templates

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(http-parser):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="http-parser" command="/usr/bin/http-parser"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libhttp-parser 2.1 http-parser (>> 2.1-0), http-parser (<< 2.1-99)
